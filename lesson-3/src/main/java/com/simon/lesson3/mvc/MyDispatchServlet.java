7df5a763d3eab8bb925a319653d9f39d8a315490
yiwen.sang桑以文 <sang.yiwen@chinaredstar.com>
2018-12-14 15:53:45 +0800

    servlet

---
+++ lesson-3/src/main/java/com/simon/lesson3/mvc/MyDispatchServlet.java
@@ -0,0 +1,34 @@
+package com.simon.lesson3.mvc;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by sang on 2018/12/11.
+ */
+public class MyDispatchServlet implements Servlet {
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+
+
+
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
